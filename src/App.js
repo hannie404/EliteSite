8e996e9da74b6297dea47205c1cca8735ee11aec
Charlie Sacro <charlacro.e@gmail.com>
2023-07-31 22:06:40 +0800

    Updated Careers Page and Work Page and organise img and css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-// import Work from './pages/Work';
-import Careers from './pages/Careers';
+import Work from './pages/Work';
+// import Careers from './pages/Careers';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
-      {/* <Work/> */}
-      <Careers/>
+      <Work/>
+      {/* <Careers/> */}
     </div>
   );
 }
